73daf1e012269c8799b27333ca59ff29d3b5e1fb
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-09 10:21:52 -0700

    Init java program

---
+++ QuickStart.java
@@ -0,0 +1,10 @@
+class QuickStart 
+{
+    public static void main(String[] args) 
+    {
+        int x = 2;
+        int y = 5;
+        System.out.println(x+y);
+        System.out.println("Hello, World.");
+    }
+}
