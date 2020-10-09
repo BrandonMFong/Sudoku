21824cbf20592eab4d904c3b774764af7d4b7ee1
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-09 11:52:25 -0700

    just using a matrix instead

--- Sudoku.java
+++ Sudoku.java
@@ -1,40 +1,49 @@
 import java.io.File; 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 class Sudoku 
 {
     
     // Print contents of array list 
     // Each entry of the list is a string array so we have to go through that string array 
-    public static void PrintBoard(ArrayList<String[]> Rows)
+    public static void PrintBoard(String [][] Board)
     {
-        for(int i = 0; i < Rows.size();i++)
+        for(int i = 0; i < 9;i++)
         {
             if(i%3 == 0) {System.out.println("\n[]=====[]=====[]=====[]");}
             else{System.out.println();}
-            for(int k = 0; k < Rows.get(i).length;k++)
+            for(int k = 0; k < 9;k++)
             {
                 if(k%3 == 0) {System.out.print("[]");}
-                System.out.print(Rows.get(i)[k]);
+                if(Board[i][k].equals("")) System.out.print(" "); // if the indexed value is empty from the csv 
+                else System.out.print(Board[i][k]);
                 if(k%3 != 2){System.out.print("|");}
             }
             System.out.print("[]");
         }
         System.out.print("\n[]=====[]=====[]=====[]\n");
     }
+
+
     public static void main(String[] args) throws FileNotFoundException 
     {
         // Load array list variable
         Scanner FileReader = new Scanner(new File(".\\res\\Matrix.csv"));
-        ArrayList<String[]> Rows = new ArrayList<String[]>();
+        // ArrayList<String[]> Board = new ArrayList<String[]>();
+        String [][] Board = new String[9][9];
         
         // go through each line
-        while (FileReader.hasNextLine()) Rows.add(FileReader.nextLine().split(",")); // Each line is an array, add that to the array list 
+        // ensuring size 9 array 
+        // Each line is an array, add that to the array list 
+        for(int i = 0; i < 9; i++) Board[i] = FileReader.nextLine().split(",",9); 
         FileReader.close();
 
-        PrintBoard(Rows);
+        PrintBoard(Board);
+
+        // Solve the board 
+
 
     }
 }
