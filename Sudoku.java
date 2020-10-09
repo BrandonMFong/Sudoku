18094618dafd06aba61fd111af8b6d613b4db0b1
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-09 10:46:31 -0700

    using arraylist

--- Sudoku.java
+++ Sudoku.java
@@ -1,26 +1,37 @@
 import java.io.File; 
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Sudoku 
 {
     public static void main(String[] args) 
     {
+        // Load array list variable
         try 
         {
-            File MyFile = new File(".\\res\\Matrix.txt");
-            Scanner FileReader = new Scanner(MyFile);
+            // File MyFile = new File(".\\res\\Matrix.csv");
+            // return new Scanner(MyFile);
+            Scanner FileReader = new Scanner(new File(".\\res\\Matrix.csv"));
+            ArrayList<String> Rows = new ArrayList<String>();
             while (FileReader.hasNextLine()) 
             {
-                String data = FileReader.nextLine();
-                System.out.println(data);
+                // String data = FileReader.nextLine();
+                // System.out.println(data);
+                Rows.add(FileReader.nextLine());
             }
             FileReader.close();
+            // Print contents of array list 
+            for(int i = 0; i < Rows.size();i++)
+            {
+                System.out.println(Rows.get(i));
+            }
         } 
         catch (FileNotFoundException e) 
         {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
     }
 }
