46a5f2ee8ff7ae57b52ba17019e2518583e88e86
BrandonMFong <fong.m.brandon97@gmail.com>
2020-10-09 11:24:25 -0700

    external catch and printing board

--- Sudoku.java
+++ Sudoku.java
@@ -5,33 +5,36 @@ import java.util.ArrayList;
 
 class Sudoku 
 {
-    public static void main(String[] args) 
+    
+    // Print contents of array list 
+    // Each entry of the list is a string array so we have to go through that string array 
+    public static void PrintBoard(ArrayList<String[]> Rows)
+    {
+        for(int i = 0; i < Rows.size();i++)
+        {
+            if(i%3 == 0) {System.out.println("\n[]=====[]=====[]=====[]");}
+            else{System.out.println();}
+            for(int k = 0; k < Rows.get(i).length;k++)
+            {
+                if(k%3 == 0) {System.out.print("[]");}
+                System.out.print(Rows.get(i)[k]);
+                if(k%3 != 2){System.out.print("|");}
+            }
+            System.out.print("[]");
+        }
+        System.out.print("\n[]=====[]=====[]=====[]\n");
+    }
+    public static void main(String[] args) throws FileNotFoundException 
     {
         // Load array list variable
-        try 
-        {
-            // File MyFile = new File(".\\res\\Matrix.csv");
-            // return new Scanner(MyFile);
-            Scanner FileReader = new Scanner(new File(".\\res\\Matrix.csv"));
-            ArrayList<String> Rows = new ArrayList<String>();
-            while (FileReader.hasNextLine()) 
-            {
-                // String data = FileReader.nextLine();
-                // System.out.println(data);
-                Rows.add(FileReader.nextLine());
-            }
-            FileReader.close();
-            // Print contents of array list 
-            for(int i = 0; i < Rows.size();i++)
-            {
-                System.out.println(Rows.get(i));
-            }
-        } 
-        catch (FileNotFoundException e) 
-        {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        Scanner FileReader = new Scanner(new File(".\\res\\Matrix.csv"));
+        ArrayList<String[]> Rows = new ArrayList<String[]>();
+        
+        // go through each line
+        while (FileReader.hasNextLine()) Rows.add(FileReader.nextLine().split(",")); // Each line is an array, add that to the array list 
+        FileReader.close();
+
+        PrintBoard(Rows);
 
     }
 }
